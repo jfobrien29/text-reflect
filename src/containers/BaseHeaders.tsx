dc1a5c5a30881af6b4b1f59d2ec6ff66bfe75cd0
jfobrien29 <thejackobrien29@gmail.com>
2021-12-16 15:30:35 -0500

    update favicon

--- src/containers/BaseHeaders.tsx
+++ src/containers/BaseHeaders.tsx
@@ -10,11 +10,30 @@ const BaseHeader: React.FC<any> = ({
 
       <title>{title}</title>
       <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" />
 
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="theme-color" content="#ffffff" />
+
+      {/** Favicon */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
 
       {/* Open Graph */}
       {/* <meta property="og:url" content={generateCurrentUrl} key="ogurl" /> */}
