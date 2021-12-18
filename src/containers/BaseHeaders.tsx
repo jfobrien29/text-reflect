a29c0c3dff81d3c4dd66121aac85f4eb5cc79a3a
jfobrien29 <thejackobrien29@gmail.com>
2021-12-17 22:57:11 -0500

    remove mobile theme color

--- src/containers/BaseHeaders.tsx
+++ src/containers/BaseHeaders.tsx
@@ -13,7 +13,6 @@ const BaseHeader: React.FC<any> = ({
 
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="theme-color" content="#ffffff" />
 
       {/** Favicon */}
       <link
